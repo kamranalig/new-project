c06a99f47f5d52d0800a93894721a0f450cd486d
Kamran Ali <km775.ali@gmail.com>
2023-06-24 18:42:26 +0500

    side bar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 // import Navbar from './component/Navbar';
 import NavbarApp from './component/navbar/NavbarApp';
+import Sidebar from './component/sidebar/Sidebar';
+import styles from "./App.module.scss";
 function App() {
   return (
     <React.Fragment>
       <NavbarApp/>
+      <div className={styles.about}>
+      <Sidebar/>
+      </div>
     {/* <Navbar/> */}
     </React.Fragment>
   )
