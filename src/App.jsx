0e3758192c6290f9d35497c7576ab0d3a4e0ab31
Kamran Ali <km775.ali@gmail.com>
2023-07-06 03:06:28 +0500

    popover

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import styles from "./App.module.scss";
-import NavbarApp from './component/navbar/NavbarApp';
-import Sidebar from './component/sidebar/Sidebar';
-import Footer from './component/footer/Footer';
-import Section from './component/section/Section';
+// import styles from "./App.module.scss";
+// import NavbarApp from './component/navbar/NavbarApp';
+// import Sidebar from './component/sidebar/Sidebar';
+// import Footer from './component/footer/Footer';
+// import Section from './component/section/Section';
+// import Slider from "./slider/slider"
+import AppPopover from './component/popover/popover';
 function App() {
   return (
     <React.Fragment>
-      <NavbarApp />
+      {/* <NavbarApp />
       <div className={styles.about}>
         <Sidebar />
         <div className={styles.border_line}></div>
         <Section/>
       </div>
-      <Footer />
+      <Footer /> */}
+      {/* <Slider/> */}
+     <AppPopover />
 
     </React.Fragment>
   )
