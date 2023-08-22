ac5974efbe0e7e4ad2d23dfc6e5a19bb623fa2d2
Kamran Ali <km775.ali@gmail.com>
2023-08-22 16:09:03 +0500

    menu detail added in this section

--- src/components/DishDetail.jsx
+++ src/components/DishDetail.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { render } from "react-dom";
 import {
   Card,
   CardImg,
@@ -24,7 +25,42 @@ const DishDetail = ({ dish }) => {
       return <div></div>;
     }
   };
-  return <div>DishDetail</div>;
+
+  const renderComments = (comments) => {
+    if (comments == null) {
+      return <div></div>;
+    }
+    const comnt = comments.map((comment) => {
+      <li key={comment.id}>
+        <p>{comment.comment}</p>
+        <p>
+          --{comment.auther}, &nbsp;
+          {new Intl.DateTimeFormat("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "2-digit",
+          }).format(new Date(comment.date))}
+        </p>
+      </li>;
+    });
+    return (
+      <div className="col-12 col-md-5 m-1">
+        <h2>Comments</h2>
+        <ul className="list-unstyles"></ul>
+      </div>
+    );
+  };
+  if (dish == null) {
+    return <div></div>;
+  }
+  const dishItem = renderDish(dish);
+  const dishComment = renderComments(dish.comments);
+  return (
+    <div className="row">
+      {dishItem}
+      {dishComment}
+    </div>
+  );
 };
 
 export default DishDetail;
