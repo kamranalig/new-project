c2a218bcb3957fcf01973afc50130c92e49bd0f1
kamran ali <km775.ali@gmail.com>
2023-12-31 23:35:01 +0500

    reducers added

---
+++ src/redux/leaders.js
@@ -0,0 +1,8 @@
+import { LEADERS } from "../shared/leader";
+
+export const Dishes = (state = LEADERS, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
---
+++ src/redux/promotions.js
@@ -0,0 +1,8 @@
+import { PROMOTIONS } from "../shared/promotion ";
+
+export const Dishes = (state = PROMOTIONS, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
