e0248e5c2e52ab48e7c6b9abfed202863eeac01d
kamran ali <km775.ali@gmail.com>
2024-01-01 02:07:53 +0500

    added the middleware in store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,10 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
+import logger from "redux-logger";
 import { Dishes } from "./dishes";
 import { Comments } from "./comments";
 import { Promotions } from "./promotions";
 import { Leaders } from "./leaders";
-import { ReactReduxContext } from "react-redux";
 
 export const ConfigureStore = () => {
   const store = createStore(
@@ -12,7 +13,8 @@ export const ConfigureStore = () => {
       comments: Comments,
       promotions: Promotions,
       leaders: Leaders,
-    })
+    }),
+    applyMiddleware(thunk, logger)
   );
   return store;
 };
